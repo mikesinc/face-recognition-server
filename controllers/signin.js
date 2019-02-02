ce4fc661754e58a12267035d73cb4370f89d8c8b
mikesinc <mas.sinclair@gmail.com>
2019-02-03 00:13:51 +0800

    remove alerts

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,7 +1,7 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
-        return alert('Please enter both email and password.');
+        return res.status(400).json('incorrect form submission');
     }
     db.select('email', 'hash').from('login')
     .where('email', '=', email)
